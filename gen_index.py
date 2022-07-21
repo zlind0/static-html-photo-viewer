06a89e77edc1a8f9ced9a3c3b879123b0248bcee
Zl <hzlclock@gmail.com>
2022-07-21 22:06:23 +0800

    prevent html selection
    Signed-off-by: Zl <hzlclock@gmail.com>

--- gen_index.py
+++ gen_index.py
@@ -5,16 +5,18 @@ import numpy as np
 import multiprocessing as mp
 import pathlib, json
 from datetime import datetime
+from tqdm import tqdm
+import shutil
 
-THUMBNAIL_WIDTH = 300
-DATE_DICT={}
+def ismedia(path):
+    return os.path.splitext(path)[1].lower() in ['.jpg', '.jpeg', '.png', '.gif', 'heic']
+
 def list_files_recursively(path):
     for root, dirs, files in os.walk(path):
         for file in files:
             res=os.path.join(root, file)
-            if 'thumbnail' not in res and 'jpg' in res:
+            if 'thumbnail' not in res and ismedia(res):
                 yield res
-IMGS=list(list_files_recursively('.'))
 
 def save_thumbnail(path, dir):
     
@@ -34,76 +36,81 @@ def save_thumbnail(path, dir):
         cv2.imwrite(dest, img, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
     except: pass
 
-
-for path in IMGS:
-    save_thumbnail(path, "thumbnails")
-
-def get_exif_shot_date(path):
-    try:
-        img = Image.open(path)
-        exif_data = datetime.strptime(img._getexif()[36867], '%Y:%m:%d %H:%M:%S')
-        return exif_data.strftime('%Y/%m/%d'), exif_data
-    except:
-        return "1970/01/01", datetime.strptime("1970/01/01", '%Y/%m/%d')
-
-for path in IMGS:
-    date, datetime = get_exif_shot_date(path)
-    print(f"path {path} date {date}")
-    if date not in DATE_DICT:
-        DATE_DICT[date] = []
-    DATE_DICT[date].append((datetime, path))
-
-THUMBNAIL_HTML=[]
-IMGS_IDS={}
-
-cnt=1
-for date in sorted(DATE_DICT.keys()):
-    DATE_DICT[date]=sorted(DATE_DICT[date], key=lambda x: x[0])
-    THUMBNAIL_HTML.append(f"<h1>{date}</h1>")
-    THUMBNAIL_HTML.append('<div class="imagegroup">')
-    for datetime, path in DATE_DICT[date]:
-        THUMBNAIL_HTML.append(f"<a href='view.html?imageid={cnt}'> <img src='thumbnails/{path}'> </a>")
-        IMGS_IDS[cnt]=path
-        cnt+=1
-    THUMBNAIL_HTML.append('</div>')
-
-HTML='''
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-</head>
-<style>
-.imagegroup {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-}
-.imagegroup img {
-    width: 50pt;
-    height: 50pt;
-}
-body{
-    font-family: sans-serif;
-}
-h1{
-    font-size: 1.5em;
-}
-</style>
-</head>
-<body>
-BODY
-</body>
-'''
-with open('index.html','w') as f:
-    f.write(HTML.replace('BODY','\n'.join(THUMBNAIL_HTML)))
-
-with open('imgs.js','w') as f:
-    f.write(f"imageData={json.dumps(IMGS_IDS)}")
+THUMBNAIL_WIDTH = 200
+DATE_DICT={}
+IMGS=list(list_files_recursively('.'))
+SCRIPT_PATH = pathlib.Path( __file__ ).absolute()
 
 
-# if __name__ == "__main__":
-#     with mp.Pool(mp.cpu_count()) as p:
-#         for path in list_files_recursively('test'):
-#             p.apply_async(save_thumbnail, args=(path, 'thumbnails'))
-#         p.close()
-#         p.join()
+if __name__ == "__main__":
+
+    with mp.Pool(2) as p:
+        for path in IMGS:
+            p.apply_async(save_thumbnail, args=(path, 'thumbnails'))
+        p.close()
+        p.join()
+
+    def get_exif_shot_date(path):
+        try:
+            img = Image.open(path)
+            exif_data = datetime.strptime(img._getexif()[36867], '%Y:%m:%d %H:%M:%S')
+            return exif_data.strftime('%Y/%m/%d'), exif_data
+        except:
+            return "1970/01/01", datetime.strptime("1970/01/01", '%Y/%m/%d')
+
+    print("READ DATE")
+    for path in tqdm(IMGS):
+        date, datetime = get_exif_shot_date(path)
+        if date not in DATE_DICT:
+            DATE_DICT[date] = []
+        DATE_DICT[date].append((datetime, path))
+
+    THUMBNAIL_HTML=[]
+    IMGS_IDS={}
+
+    cnt=1
+    for date in sorted(DATE_DICT.keys()):
+        DATE_DICT[date]=sorted(DATE_DICT[date], key=lambda x: x[0].strftime('%Y/%m/%d-%H:%M:%S')+x[1])
+        THUMBNAIL_HTML.append(f"<h1>{date}</h1>")
+        THUMBNAIL_HTML.append('<div class="imagegroup">')
+        for datetime, path in DATE_DICT[date]:
+            THUMBNAIL_HTML.append(f"<a href='view.html?imageid={cnt}' target='_blank'> <img src='thumbnails/{path}'> </a>")
+            IMGS_IDS[cnt]=path
+            cnt+=1
+        THUMBNAIL_HTML.append('</div>')
+
+    HTML='''
+    <html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    </head>
+    <style>
+    .imagegroup {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+    }
+    .imagegroup img {
+        width: 50pt;
+        height: 50pt;
+    }
+    body{
+        font-family: sans-serif;
+    }
+    h1{
+        font-size: 1.5em;
+    }
+    </style>
+    </head>
+    <body>
+    BODY
+    </body>
+    '''
+    with open('index.html','w') as f:
+        f.write(HTML.replace('BODY','\n'.join(THUMBNAIL_HTML)))
+
+    with open('imgs.js','w') as f:
+        f.write(f"imageData={json.dumps(IMGS_IDS)}")
+
+    shutil.copyfile(os.path.join(SCRIPT_PATH,"view.html"), os.path.join(os.getcwd(),"view.html"))
+# 
